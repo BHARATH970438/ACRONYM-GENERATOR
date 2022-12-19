79d6345c8cc557b381ad0e012bfcf7e7638f1bcd
Anurag <115922618+Anurag340@users.noreply.github.com>
2022-12-19 18:55:30 +0530

    Update acronym.py
    Previously if you notice every thing was written inside the function  , so it was not taking any input and the function isn't being called  --> Fixed that
    We couldn't use try nd exception in this type of program so leave it .
    Also made the comments a little bit clean and minimalistic .

--- python/acronym.py
+++ python/acronym.py
@@ -1,17 +1,22 @@
 #acronym generator
-def acronym():
-    s=input('phrase: ')  #user input, phrase to generate acronym
-    s=s.lower()   #s.lower converts user input into lower case
-    w=s.split()   #s.split converts input into list of strings
-    acronym=''        #empty string to store acronym 
-    ex=['of','an','a','the','and','&']  #exceptions not be in acronym
-    for i in w:          # for loop to extract strings from list of strings 
-        if i not in ex:  # condition to filter exceptions
-            acronym+=i[0].upper()  #it takes 1st letter in string coverts it into capital added to acronym
-    print('acronym:',acronym)      # at last it prints the output
 
+s=input('phrase: ')                           # Taking phrase as input
 
-
-        
+def acronym(string):                          # Defining a function named as acronym
+    
+    string=string.lower()   
+    w=string.split(' ')  
+    
+    acronym=''        
+    
+    ex=['of','an','a','the','and','&']        # List of strings or characters which shouldn't be in the phrase
+    
+    for i in w:
+        if i not in ex: 
+            acronym+=i[0].upper()
+    print('acronym:',acronym)                 # Printing our result
+    
+    
+acronym(s)                                    # Calling the function
 
 
